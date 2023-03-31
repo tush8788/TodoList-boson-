a6f652ed9d66762fe390c382fabc38f60f5bb04b
tush87 <tusharj8788@gmail.com>
2023-03-31 15:19:37 +0530

    add delete task func

--- controller/task_controller.js
+++ controller/task_controller.js
@@ -1,5 +1,6 @@
 const TaskDB = require('../models/task');
 
+//create task
 module.exports.addTask=async function(req,res){
     try{
         let task = await TaskDB.create({
@@ -9,6 +10,25 @@ module.exports.addTask=async function(req,res){
             user:req.body.userId
         });
 
+        return res.redirect('back');
+    }
+    catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
+}
+
+//delete task 
+module.exports.deleteTask=async function(req,res){
+    try{
+        var task = await TaskDB.findById(req.params.id);
+        // console.log(task ," ",task.user==req.user.id)
+        if(!task || req.user.id != task.user){
+            console.log("unable to delete task")
+            return res.redirect('back');
+        }
+        //delete task
+        await task.deleteOne();
         return res.redirect('back');
     }
     catch(err){
--- routes/task.js
+++ routes/task.js
@@ -3,6 +3,9 @@ const passport = require('passport');
 const router = express.Router();
 const taskController = require('../controller/task_controller');
 
+//create task
 router.post('/create',passport.checkAuthentication,taskController.addTask);
 
+//delete task
+router.get('/delete/:id',passport.checkAuthentication,taskController.deleteTask)
 module.exports = router;
