d88fce95a11e4873256c51393adb1423a8026ccb
tush87 <tusharj8788@gmail.com>
2023-03-31 20:19:43 +0530

    add create,delete and update task api

--- controller/v1/task_controller_api.js
+++ controller/v1/task_controller_api.js
@@ -16,3 +16,80 @@ module.exports.allTask= async function(req,res){
         })
     }
 }
+
+//create task
+module.exports.createTask = async function(req,res){
+    try{
+        let task = await TaskDB.create({
+            task:req.body.task,
+            category:req.body.category,
+            user:req.user.id,
+            dueDate:req.body.dueDate
+        });
+
+        return res.status(200).json({
+            message:"Task Create Successfully",
+            Task:task
+        })
+
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"Internal server error"
+        })
+    }
+}
+
+//update task
+module.exports.updateTask = async function(req,res){
+    try{
+        let Task = await TaskDB.findById(req.params.id);
+        if(Task.user == req.user.id){
+
+           await Task.updateOne(req.body);
+
+            // await TaskDB.Update(req.params.id,req.body);
+
+            return res.status(200).json({
+                message:"Update Successfully",
+                
+            })
+        }
+        else{
+            return res.status(401).json({
+                message:"Unauthorized to update task"
+            })
+        }
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"Internal server error"
+        })
+    }
+}
+
+//delete task
+module.exports.deleteTask =async function(req,res){
+    try{
+        let task = await TaskDB.findById(req.params.id);
+        if(task.user == req.user.id){
+            task.deleteOne();
+            return res.status(200).json({
+                message:"Task Delete Successfully"
+            })
+        }
+        else{
+            return res.status(401).json({
+                message:"Unauthorized to delete task"
+            })
+        }
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"Internal server error"
+        })
+    }
+}
--- routes/v1/task.js
+++ routes/v1/task.js
@@ -3,9 +3,18 @@ const router = express.Router();
 const passport = require('passport');
 const taskControllerApi = require('../../controller/v1/task_controller_api');
 
+//show all task
 router.get('/all',passport.authenticate(
     'jwt',
     {session:false}
 ),taskControllerApi.allTask);
 
+//create new task
+router.post('/create',passport.authenticate('jwt',{session:false}),taskControllerApi.createTask);
+
+//update task
+router.post('/update/:id',passport.authenticate('jwt',{session:false}),taskControllerApi.updateTask);
+
+//delete task
+router.delete('/delete/:id',passport.authenticate('jwt',{session:false}),taskControllerApi.deleteTask);
 module.exports=router;
