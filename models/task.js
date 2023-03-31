7e1a5e001e6724213545ac6844756438e72db64f
tush87 <tusharj8788@gmail.com>
2023-03-31 12:45:11 +0530

    add task schema

---
+++ models/task.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    task:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    dueDate:{
+        type:Date,
+        required:true
+    }
+},{
+    timestamps:true
+});
+
+const Task = mongoose.model('Task',taskSchema);
+
+module.exports=Task;
