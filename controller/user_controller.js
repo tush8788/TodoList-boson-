70e8607d930c225f86a192f6dc6054cd323b754d
tush87 <tusharj8788@gmail.com>
2023-03-31 11:32:37 +0530

    add signup

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,3 +1,4 @@
+const UserDB = require('../models/user');
 
 //sign up page
 module.exports.signup = function(req,res){
@@ -6,8 +7,31 @@ module.exports.signup = function(req,res){
     })
 }
 
+//sign in page
 module.exports.signin = function(req,res){
     return res.render('signin',{
         title:"Signin"
     })
+}
+
+//create user
+module.exports.createUser =async function(req,res){
+    try{
+        // console.log(req.body)
+        let user = await UserDB.findOne({email:req.body.email});
+
+        if(!user){
+            user = await UserDB.create({email:req.body.email,password:req.body.password});
+            console.log("user create Successfully");
+            return res.redirect('/user/signin');
+        }
+        else{
+            console.log("user already exist just login");
+            return res.redirect('/user/signin');
+        }
+    }
+    catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
 }
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,13 @@ const userController = require('../controller/user_controller');
 //sign up page
 router.get('/signup',userController.signup);
 
+//create user
+router.post('/create',userController.createUser);
+
 //sign in page
 router.get('/signin',userController.signin);
+
+//create session
+
 
 module.exports=router;
